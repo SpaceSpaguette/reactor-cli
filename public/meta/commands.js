e14ed65f6be1d85264eacf0ac0f9f8d2077ec1a1
Spaguette <lukasvachtl01@proton.me>
2026-03-15 17:12:44 +0100

    Corrected with vision

--- public/meta/commands.js
+++ public/meta/commands.js
@@ -6,3 +6,19 @@ function hello () {
 function loadthis (data1 = "FUCK",data2 = "YOU") {
     result.innerHTML = `${data1} ${data2} `;
 }
+
+function ChangeStation(name) {
+    console.log("changed to ", name)
+};
+
+function Exit() {
+    subSystem = "home";
+    console.log("Exited to main station");
+}
+
+function Shop (action) {
+    if (action.toLowerCase() === "buy") {
+        console.log("Buying items...");
+    } else if (action.toLowerCase() === "show") {
+        console.log("Showing items...");
+    }}
--- public/meta/inits.js
+++ public/meta/inits.js
@@ -14,54 +14,11 @@ var turbines_pressure = 0;
 
 
 //subSystem var
-var subSystem = {
-    current: "main",
-    isReady: false,
-};
-
-function ChangeStation(name) {
-    let found = true;
-
-    
-};
-
-function Exit() {
-    subSystem.current = "main";
-    console.log("Exited to main station");
-}
-
-function Shop (action) {
-    if (action.toLowerCase() === "buy") {
-        console.log("Buying items...");
-    } else if (action.toLowerCase() === "show") {
-        console.log("Showing items...");
-    }
-}
+var subSystem = 'home';
 
 // dev env == true
 var debug = true;
 
-
-
-
-// fields
-
-
+// Output options
 var history = document.getElementById("history");
 var result  = document.getElementById("result");
-
-
-
-
-
-// very sensitive. please ensure you dont touch it. LOADS COMMANDS INTO MEMORY
-let commands = {};
-fetch("meta/data/commands.json")
-    .then(r => r.json())
-    .then(data => {
-        commands = data;
-        console.log("Commands loaded into memory:");
-    })
-    .catch(err => console.error("Failed to load commands:", err));
-
-
