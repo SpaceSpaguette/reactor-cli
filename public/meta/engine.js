8dc8b40d08775bbc1679fe65db9e992404eb606e
Spaguette <lukasvachtl01@proton.me>
2026-04-14 12:53:56 +0200

    added things from experminal D

--- public/meta/engine.js
+++ public/meta/engine.js
@@ -191,3 +191,35 @@ function temperature() {
     ▌
 
 */
+/*
+
+
+|-----------------------|
+|   This section is     |    
+|   responsible for     |
+|   changing the display|
+|   mess with it and    |
+|   youre dead          |
+|-----------------------|
+*/
+
+async function updateDisplay () {
+    // Here we get all the values to display
+    const fueldis       = document.getElementById('fuelaverage')
+    const t2refuel      = document.getElementById('ttrefuel')
+    // END OF FUEL
+    // START OF COOLER
+    const coollevel     = document.getElementById('cooler')
+    const refridge      = document.getElementById('rfridge')
+    const epsr_display  = document.getElementById('furesupress')
+    // END OF COOLER
+    //START OF TEMPERATURE
+    const display_heat  = document.getElementById('heatg')
+    const coolrate      = document.getElementById('coolrate')
+    const display_heath = document.getElementById('health')
+
+    
+
+
+
+}   
